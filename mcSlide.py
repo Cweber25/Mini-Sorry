b005c14e53adbc044043fb6421bab8975d8b4499
Cole <Onesockweber@gmail.com>
2023-06-01 01:15:24 -0400

    All of the slide stuff was added. If it lands on 5 it slides to 8 and if its a 3 it can be split.

---
+++ mcSlide.py
@@ -0,0 +1,74 @@
+import random
+import statistics
+
+def main():
+    # User input for the number of turns
+    amount = int(input("Enter the number of turns: "))
+
+    # Initialize counters for card counts and reshuffles
+    spaceCounters = [[0, 0, 0, 0] for _ in range(14)]  # Counters for each space and card [space][card]
+    emptyCountTotal = 0
+
+    # Run the game simulation and collect statistics
+    totalArr, spaceCounters, emptyCountTotal, slideCount = miniSorry(amount, spaceCounters)
+
+    # Print game statistics
+    print("Game Statistics")
+    print("----------------")
+    print(f"Average game length: {statistics.mean(totalArr):.3f}")
+    print(f"Standard deviation of game length: {statistics.stdev(totalArr):.3f}")
+    print(f"Number of times the deck was reshuffled: {emptyCountTotal}")
+    print(f"Number of times the slide from space 5 to space 8 was used: {slideCount}")
+    print("\nCard counts and proportions for each card at each space:")
+    print("--------------------------------------------------------")
+
+    # Print card counts and proportions for each space
+    for space, counters in enumerate(spaceCounters):
+        total = sum(counters)
+        proportions = [count / total if total != 0 else 0 for count in counters]
+        print(f"Space {space}:")
+        print(f"  Card Counts - 1: {counters[0]}, 2: {counters[1]}, 3: {counters[2]}, 4: {counters[3]}")
+        print(f"  Proportions - 1: {proportions[0]:.4f}, 2: {proportions[1]:.4f}, 3: {proportions[2]:.4f}, 4: {proportions[3]:.4f}")
+        print()
+
+    print(f"The deck was reshuffled {emptyCountTotal} time(s).\n")
+
+
+def miniSorry(amount, spaceCounters):
+    totalArr = []  # List to store game lengths
+    emptyCount = 0  # Counter for deck reshuffles
+    slideCount = 0  # Counter for slide usage
+
+    for _ in range(amount):
+        turns = 0
+        spot = 0
+        deck = [1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4]  # Initial deck of cards
+        deckOriginal = deck[:]  # Make a copy of the original deck
+
+        while spot != 13:
+            ran = random.choice(deck)  # Draw a card randomly from the deck
+            spaceCounters[spot][ran - 1] += 1  # Increment the counter for the card at the current space
+
+            if  spot + ran == 5:  # Special case: slide from space 5 to space 8
+                spot = 8
+                slideCount += 1
+            else:
+                spot += ran
+
+            if spot > 13:
+                spot -= ran  # Adjust the spot if it exceeds 13 (end space)
+            deck.remove(ran)  # Remove the drawn card from the deck
+
+            if not deck:  # Check if the deck is empty
+                deck = deckOriginal[:]  # Reshuffle the deck by restoring the original deck
+                emptyCount += 1
+
+            turns += 1  # Increment the turn count
+
+        totalArr.append(turns)  # Store the game length
+
+    return totalArr, spaceCounters, emptyCount, slideCount
+
+
+if __name__ == '__main__':
+    main()
